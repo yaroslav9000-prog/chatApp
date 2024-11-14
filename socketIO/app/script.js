22c1dcaee6fbcd756ef5cd96036b0ab984f7cb5b
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-11-15 01:53:26 +0200

    didnt pay attention to i destrcuturing message object passed from socket server

--- socketIO/app/script.js
+++ socketIO/app/script.js
@@ -1,25 +1,26 @@
-const socket = io('ws://localhost:8080');
+const socket = io('http://localhost:8080');
 
 function sendMessage(event){
     event.preventDefault();
     const input = document.querySelector('input[type=text]');
     if(input.value){
-        socket.emit(input.value);
+        socket.emit('message' ,input.value);
         const message = input.value;
         const myList = document.querySelector('ul');
         const newMessage = document.createElement('li');
         newMessage.textContent = message;
         myList.appendChild(newMessage);
-        message.value = '';
+        input.value = '';
     }
-    message.focus;
+    input.focus();
     
 }
 
 document.querySelector('form')
 .addEventListener('submit', sendMessage)
 
-socket.on('message', ({data})=>{
+socket.on("message", data=>{
+    console.log(data);
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
--- socketIO/server/index.js
+++ socketIO/server/index.js
@@ -15,15 +15,10 @@ const io = new Server(httpServer, {
 io.on("connection", socket=>{
     console.log(`User ${socket.id} connected`);
 
-    socket.on('message', message=>{
+    socket.on("message", message=>{
         console.log(message);
-        io.emit(`message`, `${socket.id.substring(0,5)}: ${message}`);
+        io.emit("message", `${socket.id.substring(0,5)}: ${message}`);
     })
-
-    socket.on('close', ()=>{
-        console.log("User disconnected");
-    })
-    
 })
 
 
