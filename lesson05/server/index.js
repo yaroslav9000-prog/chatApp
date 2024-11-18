600b974346ef7f2a5d112f9d9a9e7875e10b9fe4
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-11-18 02:02:15 +0200

    still no clue how to start server with wss

--- lesson05/server/index.js
+++ lesson05/server/index.js
@@ -1,47 +1,21 @@
 import express from "express";
-import {Server} from "socket.io";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import ws, {WebSocketServer} from "ws";
 
 const PORT = process.env.PORT || 3500;
 
-const app = express();
+const app = express(); 
 
-const expressServer = app.listen(PORT, ()=>{
-    console.log('listening on port ', PORT);
+const wss = new WebSocketServer();
+
+wss.on('connection', socket=>{
+    socket.on('error', console.error);
+
+    socket.on('message', (data)=>{
+        console.log(data);
+        socket.send(`I got a message from you was it: ${data}`);
+    })
 })
 
-app.use(express.static(path.join(__dirname,'public')));
-
-const io = new Server(expressServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false: ["http://localhost:5500", "http://127.0.0.1:5500"]
-    }
-});
-
-
-io.on("connection", socket=>{
-    console.log(`User ${socket.id} connected`);
-
-    //Upon signle user connecting
-    socket.emit('message', 'Welcome to the chat');
-    //Upon connection - to all users.
-    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`);
-
-    socket.on("message", message=>{
-        console.log(message);
-        io.emit("message", `${socket.id.substring(0,5)}: ${message}`);
-    })
-
-    socket.on('disconnect', ()=>{
-        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`);
-    })
-
-    socket.on('typing', (name)=>{
-        socket.broadcast.emit('typing', name)
-    })
+app.listen(PORT, ()=>{
+    console.log('server is up and running on port ', PORT);
 })
--- lesson05/server/public/script.js
+++ lesson05/server/public/script.js
@@ -1,2 +1,26 @@
-// const socket = io("http://localhost:3500");
+const ws = new WebSocket('http://localhost:3500');
+
+const nameInput = document.querySelector('.nameInput');
+const roomInput = document.querySelector('.roomInput');
+const messageList = document.querySelector('.messageList');
+const messageInput = document.querySelector('.messageInput')
+const sendMessageButton = document.querySelector('.sendMessage');
+
+
+const sendMessage = () =>{
+    const message = messageInput.textContent;
+    ws.send(message);
+    console.log('message was sent to a websocket ...', message)
+}
+sendMessageButton.addEventListener('click', ()=>{
+    sendMessage();
+})
+
+ws.onerror((error)=>{
+    console.log(error);
+})
+
+ws.on('message', ()=>{
+
+})
 
